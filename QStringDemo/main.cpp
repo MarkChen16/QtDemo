70dd9249ce9d74d8da40a1a9afe6ee1155bc082b
Mark Chen <guiquanchen@qq.com>
2019-01-23 17:28:10 +0800

    20190123

--- QStringDemo/main.cpp
+++ QStringDemo/main.cpp
@@ -23,6 +23,29 @@ int main(int argc, char *argv[])
 
 	int k = str3.toInt(nullptr, 8);
 
+	//常用功能
+	str1.prepend("开始");
+	str1.append("结束");
+
+	str1 = str1.toUpper();
+	str1 = str1.toLower();
+
+	int i1 = str1.count();
+	int i2 = str1.size();
+	int i3 = str1.length();
+
+	str2 = "Are    you    OK?  ";
+	str1 = str2.trimmed();
+	str1 = str2.simplified();
+
+	str3 = "C:\\123\\456\\123\\Info.txt";
+	int i4 = str3.indexOf("123", 0, Qt::CaseInsensitive);	//不区分大小写
+	int i5 = str3.lastIndexOf("\\");
+
+	bool b1 = str3.contains("456");
+
+
+
 
 	return a.exec();
 }
